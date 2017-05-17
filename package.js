af209ffa9ba7b44f80a07921a16d315c393714d9
dr.dimitru <ceo@veliov.com>
2017-05-17 07:29:05 +0300

    v1.1.0
     - Compatibility with latest Meteor release
     - No dependencies
     - Better tests
     - ES6 Syntax & Import
     - __Breaking:__ `AESencrypt` and `AESdecrypt` doesn't exposed to
    global scope, use - ES6 `import`

--- package.js
+++ package.js
@@ -1,21 +1,19 @@
 Package.describe({
   name: 'ostrio:aes-crypto',
-  version: '1.0.0',
+  version: '1.1.0',
   summary: 'Simplified isomorphic API for AES cipher by CryptoJS',
   git: 'https://github.com/VeliovGroup/meteor-aes-crypto',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2');
-  api.use('ostrio:base64@1.0.0');
-  api.addFiles('aes-crypto.js');
-  api.export('AESencrypt');
-  api.export('AESdecrypt');
+  api.versionsFrom('1.4');
+  api.use('ecmascript');
+  api.mainModule('aes-crypto.js');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use(['ecmascript', 'tinytest']);
   api.use('ostrio:aes-crypto');
   api.addFiles('aes-crypto-tests.js');
 });
